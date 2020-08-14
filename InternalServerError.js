97f7e12ca6655a095274272d552fb21f6875fe75
twinh <twinhuang@qq.com>
2020-08-14 17:12:33 +0800

    style(eslint): single quotes, double jsx-quotes

--- InternalServerError.js
+++ InternalServerError.js
@@ -1,6 +1,6 @@
-import React from "react";
-import {Result} from "antd";
-import {BtnLink} from "@mxjs/a-button";
+import React from 'react';
+import {Result} from 'antd';
+import {BtnLink} from '@mxjs/a-button';
 
 export default (props) => (
   <Result
--- Ret.js
+++ Ret.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import styled from '@emotion/styled';
 import {Flex, Heading, Text, Image as BaseImage} from 'rebass';
 
