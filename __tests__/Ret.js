d3d1c4bef052f933aaec8a5dcde00df776be2f97
twinh <twinhuang@qq.com>
2021-04-30 10:14:24 +0800

    test(Ret): 初始化测试

---
+++ __tests__/Ret.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Ret} from '..';
+import {render} from '@testing-library/react';
+import {Ret as RetObject} from 'miaoxing';
+
+describe('Ret', () => {
+  test('basic', () => {
+    const result = render(<Ret ret={RetObject.new({code: 1, message: 'error'})}/>);
+    expect(result.container).toMatchSnapshot();
+  });
+});
